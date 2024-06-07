61e646ea64f5fd53563defc08a955daf372d9923
Choy-Mutao <zcy66751@163.com>
2024-06-07 15:50:42 +0800

    add Demo1.cs

--- CancellationTokenDemo/CancellationTokenDemo/Demo1.cs
+++ CancellationTokenDemo/CancellationTokenDemo/Demo1.cs
@@ -1,26 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CancellationTokenDemo
+﻿namespace CancellationTokenDemo
 {
     /// <summary>
     /// 本 demo 旨在实现 [操作取消, 即调用取消函数, 实现异步的停止]
     /// </summary>
     public class Demo1
     {
-        private CancellationTokenSource m_cancellationTokenSource = new CancellationTokenSource();
+        // CS0236 A field initializer cannot reference the non-static field, method
+        private static CancellationTokenSource m_cancellationTokenSource = new CancellationTokenSource();
+        private CancellationToken m_cur_cancellationtoken = m_cancellationTokenSource.Token;
 
         public void Start()
         {
+            Task.Run(() =>
+            {
+                while (!m_cur_cancellationtoken.IsCancellationRequested)
+                {
+                    Console.WriteLine("\n I m running");
+                    Thread.Sleep(1000);
+                }
 
+                Console.WriteLine("\n I m cancelled");
+            });
         }
 
-        public void Cancel()
-        {
-
-        }
+        public void Cancel() =>  m_cancellationTokenSource.Cancel();
     }
 }
--- CancellationTokenDemo/CancellationTokenDemo/Program.cs
+++ CancellationTokenDemo/CancellationTokenDemo/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CancellationTokenDemo;
+
 Console.WriteLine("Hello, World!");
+
+var cancel_demo1 = new Demo1();
+cancel_demo1.Start();
+
+// 等待用户输入以取消任务
+Console.ReadKey();
+
+cancel_demo1.Cancel();
